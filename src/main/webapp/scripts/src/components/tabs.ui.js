d99ba5a29aff7b7efca07fec7b28857ae39e698b
z827101859 <15669073978@163.com>
2017-06-08 14:16:28 +0800

    change:tabs改为使用SharkUI对象初始化

--- src/main/webapp/scripts/src/components/tabs.ui.js
+++ src/main/webapp/scripts/src/components/tabs.ui.js
@@ -10,8 +10,8 @@ var Templates = require('../common/templates');
 var template = Templates.tabs;
 var templateFun = Templates.templateAoT(template);
 // 初始化tabs的dom
-function initDom(sharkComponent, config) {
-    if (this === $.fn) {
+function initDom(sharkComponent, config, targetElement) {
+    if (!targetElement) {
         sharkComponent.createType = 'construct';
         var fun = config.dom ? Templates.templateAoT(config.dom) : templateFun;
         var html = fun.apply(config);
@@ -26,7 +26,7 @@ function initDom(sharkComponent, config) {
 // 初始化事件
 function initEvents(sharkComponent, config) {
     var tabs = sharkComponent.component;
-    tabs.on('click.tabs', '.nav-tabs li', BaseComponent.filterComponentAction(sharkComponent, function(e) {
+    tabs.on('click.tabs', '.nav-tabs li', BaseComponent.filterComponentAction(sharkComponent, function (e) {
         var index = $(this).index();
         switchTo(sharkComponent, index, config.onTabSwitch);
     }));
@@ -52,11 +52,11 @@ function switchTo(sharkComponent, index, cb) {
 function startAutoSwitch(sharkComponent, config) {
     var tabs = sharkComponent.component;
     doAutoSwitch(sharkComponent, config);
-    tabs.on('mouseover.tabs', function() {
+    tabs.on('mouseover.tabs', function () {
         clearInterval(sharkComponent.autoSwitchTimer);
         sharkComponent.autoSwitchTimer = null;
     });
-    tabs.on('mouseout.tabs', function() {
+    tabs.on('mouseout.tabs', function () {
         doAutoSwitch(sharkComponent, config);
     });
 }
@@ -64,7 +64,7 @@ function startAutoSwitch(sharkComponent, config) {
 function doAutoSwitch(sharkComponent, config) {
     var tabs = sharkComponent.component;
     var menu = tabs.find('.nav-tabs');
-    sharkComponent.autoSwitchTimer = setInterval(function() {
+    sharkComponent.autoSwitchTimer = setInterval(function () {
         var index = menu.find('li.active').index() + 1;
         switchTo(sharkComponent, index, config.onTabSwitch);
     }, config.auto);
@@ -77,56 +77,54 @@ function stopAutoSwitch(sharkComponent) {
     tabs.off('mouseover.tabs').off('mouseout.tabs');
 }
 
-$.fn.extend({
-    sharkTabs: function(options) {
-        /*********默认参数配置*************/
-        var config = {
-            tabs: [],
-            initTab: 0,
-            dom: '',
-            onTabSwitch: function() {}
-        };
-        SharkUI.extend(config, options);
-        /*********初始化组件*************/
-        var sharkComponent = {};
-        initDom.call(this, sharkComponent, config);
-        BaseComponent.addComponentBaseFn(sharkComponent, config);
-        initEvents(sharkComponent, config);
-        switchTo(sharkComponent, config.initTab);
-        //切换至某个tab
-        sharkComponent.switchTo = function(index, cb) {
-            var callback;
-            if (cb === true) {
-                callback = config.onTabSwitch;
-            } else if (typeof cb === 'function') {
-                callback = cb;
-            } else {
-                callback = false;
-            }
-            switchTo(sharkComponent, index, callback);
-        };
-        //开启自动切换
-        sharkComponent.startAutoSwitch = function(auto) {
-            if (/^[1-9]{1,}[0-9]*$/.test(auto)) {
-                //正整数
-                config.auto = auto;
-                startAutoSwitch(sharkComponent, config);
-            }
-        };
-        //关闭自动切换
-        sharkComponent.stopAutoSwitch = function() {
-            stopAutoSwitch(sharkComponent);
-        };
-        //销毁
-        sharkComponent.destroy = function() {
-            stopAutoSwitch(sharkComponent);
-            if (sharkComponent.createType === 'construct') {
-                sharkComponent.component.remove();
-            } else {
-                sharkComponent.component.off('click.tabs');
-            }
-            sharkComponent = null;
-        };
-        return sharkComponent;
-    }
-});
+SharkUI.sharkTabs = function (options, targetElement) {
+    /*********默认参数配置*************/
+    var config = {
+        tabs: [],
+        initTab: 0,
+        dom: '',
+        onTabSwitch: function () { }
+    };
+    SharkUI.extend(config, options);
+    /*********初始化组件*************/
+    var sharkComponent = {};
+    initDom(sharkComponent, config, targetElement);
+    BaseComponent.addComponentBaseFn(sharkComponent, config);
+    initEvents(sharkComponent, config);
+    switchTo(sharkComponent, config.initTab);
+    //切换至某个tab
+    sharkComponent.switchTo = function (index, cb) {
+        var callback;
+        if (cb === true) {
+            callback = config.onTabSwitch;
+        } else if (typeof cb === 'function') {
+            callback = cb;
+        } else {
+            callback = false;
+        }
+        switchTo(sharkComponent, index, callback);
+    };
+    //开启自动切换
+    sharkComponent.startAutoSwitch = function (auto) {
+        if (/^[1-9]{1,}[0-9]*$/.test(auto)) {
+            //正整数
+            config.auto = auto;
+            startAutoSwitch(sharkComponent, config);
+        }
+    };
+    //关闭自动切换
+    sharkComponent.stopAutoSwitch = function () {
+        stopAutoSwitch(sharkComponent);
+    };
+    //销毁
+    sharkComponent.destroy = function () {
+        stopAutoSwitch(sharkComponent);
+        if (sharkComponent.createType === 'construct') {
+            sharkComponent.component.remove();
+        } else {
+            sharkComponent.component.off('click.tabs');
+        }
+        sharkComponent = null;
+    };
+    return sharkComponent;
+}
