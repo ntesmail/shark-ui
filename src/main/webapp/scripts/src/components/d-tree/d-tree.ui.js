88201b9547d9c980c278cef79588e6c7bf57b821
凌巧 <hzlingqiao@corp.netease.com>
2017-07-11 16:24:35 +0800

    change:将对数据树的处理单独提取出来

--- src/main/webapp/scripts/src/components/d-tree/d-tree.ui.js
+++ src/main/webapp/scripts/src/components/d-tree/d-tree.ui.js
@@ -6,6 +6,7 @@ import $ from 'jquery';
 import { SharkUI } from '../../common/core';
 import { BaseComponent } from '../../common/base';
 import { Diff } from './diff';
+import { DataProcess } from './data-process';
 
 function patchs(node, walker, patches) {
     var currentPatches = patches[walker.index];
@@ -106,67 +107,6 @@ function reOrderChildren(node, moves) {
     }
 }
 
-// 通过count来决定元素的选中状态, 父级checked状态不对，可以在此修正
-function setStateByCount(node, children) {
-    var checkedCount = 0;
-    var len = children.length;
-    for (var i = 0; i < len; i++) {
-        var child = children[i];
-        // 如果是存在子节点是半选状态，则父节点一定是半选状态，没有必要再循环下去，因此结束循环
-        if (child.state === 1) {
-            checkedCount = 'minus';
-            break;
-        }
-        if (child.checked) {
-            checkedCount++;
-        }
-    }
-    switch (checkedCount) {
-        case 0:
-            node.state = 0;
-            node.checked = false;
-            break;
-        case len:
-            node.state = 2;
-            node.checked = true;
-            break;
-        default:
-            node.state = 1;
-            node.checked = false;
-    }
-}
-
-// 得到node的选中状态(选中/未选中/半选)
-function getNodeState(node, children) {
-    if (children) { // 存在子节点，由子节点决定state状态
-        // 通过count来决定元素的选中状态
-        setStateByCount(node, children);
-    } else { // 不存在子节点，由自身的checked状态来决定state状态
-        node.state = node.checked ? 2 : 0;
-    }
-}
-
-// 处理节点，为每个节点加上count属性，父节点id和选中状态 | (做递归处理)
-function handleNode(node) {
-    var children = node.children;
-    node.count = 0;
-    children && children.forEach(function (child) {
-        handleNode(child);
-        child.parentId = node.node_id;
-        node.count += child.count + 1;
-    });
-    // 得到node的选中状态(选中/未选中/半选)
-    getNodeState(node, children);
-}
-
-// 获取数据根节点
-function getTopNode(nodes) {
-    var topNode = { children: nodes };
-    // 处理节点，为每个节点加上count属性和父节点id和选中状态
-    handleNode(topNode);
-    return topNode;
-}
-
 // 获取node的dom节点
 function getNodeDom(node) {
     var children = node.children;
@@ -212,74 +152,6 @@ function initDom(sharkComponent, targetElement) {
     sharkComponent.component = component;
 }
 
-// 修改子集的选中状态
-function changeChildren(children, checked) {
-    children.forEach(function (child) {
-        child.checked = checked;
-        child.state = checked ? 2 : 0;
-        var iChildren = child.children;
-        iChildren && changeChildren(iChildren, checked);
-    });
-}
-
-// 修改父集的选中状态
-function changeParent(newTopNode, node, id) {
-    var children = node.children || [];
-    if (node.node_id === id) {
-        setStateByCount(node, children);
-        // 检查是否还存在父级
-        node.parentId && changeParent(newTopNode, newTopNode, node.parentId);
-        return node;
-    } else {
-        for (var i = 0; i < children.length; i++) {
-            var node = changeParent(newTopNode, children[i], id);
-            if (node) {
-                return node;
-            }
-        }
-    }
-}
-
-// 修改数据树的选中状态
-function changeChecked(newTopNode, node, id) {
-    var children = node.children || [];
-    if (node.node_id === id) {
-        // 切换节点checked状态
-        node.checked = !node.checked;
-        node.state = node.checked ? 2 : 0;
-        // 子集的checked属性与父级保持一致
-        changeChildren(children, node.checked);
-        changeParent(newTopNode, newTopNode, node.parentId);
-        return node;
-    } else {
-        for (var i = 0; i < children.length; i++) {
-            var node = changeChecked(newTopNode, children[i], id);
-            // 如果node存在，没有必要再循环下去，直接返回
-            if (node) {
-                return node;
-            }
-        }
-    }
-}
-
-// 修改数据树的选中状态
-function changeOpen(node, id) {
-    var children = node.children || [];
-    if (node.node_id === id) {
-        // 切换节点checked状态
-        node.open = !node.open;
-        return node;
-    } else {
-        for (var i = 0; i < children.length; i++) {
-            var node = changeOpen(children[i], id);
-            // 如果node存在，没有必要再循环下去，直接返回
-            if (node) {
-                return node;
-            }
-        }
-    }
-}
-
 // 初始化事件
 function initEvents(sharkComponent) {
     var component = sharkComponent.component;
@@ -290,7 +162,7 @@ function initEvents(sharkComponent) {
         var newTopNode = {};
         SharkUI.extend(newTopNode, sharkComponent.topNode);
         // 修改新的数据树的选中状态
-        changeChecked(newTopNode, newTopNode, id);
+        DataProcess.changeChecked(newTopNode, newTopNode, id);
         // 得到两棵数据树的差异
         var patches = Diff.diff(sharkComponent.topNode, newTopNode);
         patchs(component, { index: 0 }, patches);
@@ -305,7 +177,7 @@ function initEvents(sharkComponent) {
         var newTopNode = {};
         SharkUI.extend(newTopNode, sharkComponent.topNode);
         // 修改展开收起的状态
-        changeOpen(newTopNode, id);
+        DataProcess.changeOpen(newTopNode, id);
         // 得到两棵数据树的差异
         var patches = Diff.diff(sharkComponent.topNode, newTopNode);
         patchs(component, { index: 0 }, patches);
@@ -317,10 +189,10 @@ function initEvents(sharkComponent) {
 
 // 重新render
 function render(sharkComponent, newTreeData) {
-    var topNode = getTopNode(newTreeData);
-    var patches = Diff.diff(sharkComponent.topNode, topNode);
+    var newTopNode = DataProcess.getTopNode(newTreeData);
+    var patches = Diff.diff(sharkComponent.topNode, newTopNode);
     patchs(sharkComponent.component, { index: 0 }, patches);
-    sharkComponent.topNode = topNode;
+    sharkComponent.topNode = newTopNode;
 }
 
 SharkUI.sharkDTree = function (options, targetElement) {
@@ -331,7 +203,7 @@ SharkUI.sharkDTree = function (options, targetElement) {
     // 组件对象
     var sharkComponent = {};
     // 获取数据根节点
-    sharkComponent.topNode = getTopNode(config.nodes);
+    sharkComponent.topNode = DataProcess.getTopNode(config.nodes);
     // 初始化dom节点
     initDom(sharkComponent, targetElement);
     // 添加基础方法
