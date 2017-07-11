0a0b5130605ae6dd98776ea82b532cf5caa95738
凌巧 <hzlingqiao@corp.netease.com>
2017-07-11 16:07:35 +0800

    change:提取公共部分

--- src/main/webapp/scripts/src/components/d-tree/d-tree.ui.js
+++ src/main/webapp/scripts/src/components/d-tree/d-tree.ui.js
@@ -106,42 +106,47 @@ function reOrderChildren(node, moves) {
     }
 }
 
-function changeStateByCount(node, count, len) {
-    switch (count) {
+// 通过count来决定元素的选中状态, 父级checked状态不对，可以在此修正
+function setStateByCount(node, children) {
+    var checkedCount = 0;
+    var len = children.length;
+    for (var i = 0; i < len; i++) {
+        var child = children[i];
+        // 如果是存在子节点是半选状态，则父节点一定是半选状态，没有必要再循环下去，因此结束循环
+        if (child.state === 1) {
+            checkedCount = 'minus';
+            break;
+        }
+        if (child.checked) {
+            checkedCount++;
+        }
+    }
+    switch (checkedCount) {
         case 0:
             node.state = 0;
+            node.checked = false;
             break;
         case len:
             node.state = 2;
+            node.checked = true;
             break;
         default:
             node.state = 1;
+            node.checked = false;
     }
 }
 
-// 根据子节点的选中情况，得到node的选中状态
-function getNodeState(node) {
-    var children = node.children;
-    if (children) {
-        var count = 0;
-        var len = children.length;
-        for (var i = 0; i < len; i++) {
-            var child = children[i];
-            if (child.state === 1) {
-                count = 'minus';
-                break;
-            }
-            if (child.checked) {
-                count++;
-            }
-        }
-        changeStateByCount(node, count, len);
-    } else {
+// 得到node的选中状态(选中/未选中/半选)
+function getNodeState(node, children) {
+    if (children) { // 存在子节点，由子节点决定state状态
+        // 通过count来决定元素的选中状态
+        setStateByCount(node, children);
+    } else { // 不存在子节点，由自身的checked状态来决定state状态
         node.state = node.checked ? 2 : 0;
     }
 }
 
-// 处理节点，为每个节点加上count属性，父节点id和选中状态
+// 处理节点，为每个节点加上count属性，父节点id和选中状态 | (做递归处理)
 function handleNode(node) {
     var children = node.children;
     node.count = 0;
@@ -150,14 +155,14 @@ function handleNode(node) {
         child.parentId = node.node_id;
         node.count += child.count + 1;
     });
-    // 根据子节点的选中情况，得到node的选中状态
-    getNodeState(node);
+    // 得到node的选中状态(选中/未选中/半选)
+    getNodeState(node, children);
 }
 
 // 获取数据根节点
 function getTopNode(nodes) {
     var topNode = { children: nodes };
-    // 处理节点，为每个节点加上count属性和父节点id
+    // 处理节点，为每个节点加上count属性和父节点id和选中状态
     handleNode(topNode);
     return topNode;
 }
@@ -221,24 +226,7 @@ function changeChildren(children, checked) {
 function changeParent(newTopNode, node, id) {
     var children = node.children || [];
     if (node.node_id === id) {
-        var checked = true;
-        var len = children.length;
-        var count = 0;
-        for (var i = 0; i < len; i++) {
-            // 只要有一个是false，就是false
-            if (!children[i].checked) {
-                checked = false;
-            } else {
-                count++;
-            }
-            if (children[i].state === 1) {
-                checked = false;
-                count = 'minus';
-                break;
-            }
-        }
-        changeStateByCount(node, count, len);
-        node.checked = checked;
+        setStateByCount(node, children);
         // 检查是否还存在父级
         node.parentId && changeParent(newTopNode, newTopNode, node.parentId);
         return node;
@@ -295,6 +283,7 @@ function changeOpen(node, id) {
 // 初始化事件
 function initEvents(sharkComponent) {
     var component = sharkComponent.component;
+    // 选中/取消勾选
     component.on('click', 'li', function (e) {
         var li = $(e.currentTarget);
         var id = li.data('id');
@@ -309,7 +298,7 @@ function initEvents(sharkComponent) {
         // 阻止冒泡
         e.stopPropagation();
     });
-
+    // 展开/收起
     component.on('click', 'i', function (e) {
         var li = $(e.currentTarget).parent('li');
         var id = li.data('id');
