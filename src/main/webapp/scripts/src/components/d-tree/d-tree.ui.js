2252d3839ce7e7db5f470cd4f7442224c5b850aa
凌巧 <hzlingqiao@corp.netease.com>
2017-07-14 21:13:14 +0800

    change:selectable为false时不加tree-node-selected

--- src/main/webapp/scripts/src/components/d-tree/d-tree.ui.js
+++ src/main/webapp/scripts/src/components/d-tree/d-tree.ui.js
@@ -18,15 +18,15 @@ function initEvents(sharkComponent, config) {
         var target = $(evt.target);
         if (target.hasClass('tree-switcher')) { // 修改展开收起的状态
             var node = TreeData.changeOpen(newTopNode, id);
-            compareAndRender(sharkComponent, newTopNode, config.checkable);
+            compareAndRender(sharkComponent, newTopNode, config);
             config.onExpand.call(sharkComponent, node, node.open);
         } else if (!target.hasClass('disabled') && target.hasClass('tree-checkbox') && config.checkable) {  // 修改新的数据树的选中状态
             var node = TreeData.toggleCheck(newTopNode, id, config);
-            compareAndRender(sharkComponent, newTopNode, config.checkable);
+            compareAndRender(sharkComponent, newTopNode, config);
             config.onNodeChecked.call(sharkComponent, node, node.checked);
         } else if (!target.hasClass('disabled') && target.hasClass('tree-title') && config.selectable) {
             var node = TreeData.toggleSelect(newTopNode, id, config);
-            compareAndRender(sharkComponent, newTopNode, config.checkable);
+            compareAndRender(sharkComponent, newTopNode, config);
             config.onNodeSelected.call(sharkComponent, node, node.selected);
         }
         // 阻止冒泡
@@ -35,17 +35,17 @@ function initEvents(sharkComponent, config) {
 }
 
 // 比较两棵数据树的差异，并且渲染
-function compareAndRender(sharkComponent, newTopNode, checkable) {
+function compareAndRender(sharkComponent, newTopNode, config) {
     // 得到两棵数据树的差异
     var patches = Diff.diff(sharkComponent.topNode, newTopNode);
-    TreeDom.applyToTree(sharkComponent.component, { index: 0 }, patches, checkable);
+    TreeDom.applyToTree(sharkComponent.component, { index: 0 }, patches, config);
     sharkComponent.topNode = newTopNode;
 }
 
 function checkAll(sharkComponent, flag, config) {
     var newTopNode = SharkUI.extend({}, sharkComponent.topNode);
     TreeData.checkAll(newTopNode, flag);
-    compareAndRender(sharkComponent, newTopNode, config.checkable);
+    compareAndRender(sharkComponent, newTopNode, config);
 }
 
 SharkUI.sharkDTree = function (options, targetElement) {
@@ -55,32 +55,33 @@ SharkUI.sharkDTree = function (options, targetElement) {
         nodes: [], // 树数据
         openAll: true, // 是否全部展开，默认true
         link: true, // 父子级节点是否关联，默认为true
-        selectable: false,
-        disabled: false,
-        multiple: false,
-        checkable: true,
-        onExpand: function () { },
-        onNodeChecked: function () { }, // check之后的回调
-        onNodeSelected: function () { }
+        selectable: false, // 节点是否可选中，默认为false
+        disabled: false, // 节点是否禁用，默认为false
+        multiple: false, // 节点是否可多选，默认为true
+        checkable: true, // 是否有checkbox，默认为true
+        onExpand: function () { }, // 节点展开时的回调
+        onNodeChecked: function () { }, // 节点选中的回调
+        onNodeSelected: function () { } // checkbox选中的回调
     };
     SharkUI.extend(config, options);
     // 添加基础方法
     BaseComponent.addComponentBaseFn(sharkComponent, config);
     // 获取经过一系列处理的数据根节点
     sharkComponent.topNode = TreeData.getTopNode(config.nodes, config);
-    // 是否全部展开，如果是则重新处理数据树
+    // 是否全部展开，如果是，重新处理数据树
     if (config.openAll) {
         TreeData.openAll(sharkComponent.topNode);
     }
-    // 全部不能选
+    // 节点全部不能选
     if (config.disabled) {
         TreeData.disabledAll(sharkComponent.topNode);
     }
+    // checkbox全部不能选
     if (config.disableCheckbox) {
         TreeData.disableCheckboxAll(sharkComponent.topNode);
     }
     // 初始化dom节点
-    sharkComponent.component = TreeDom.initDom(sharkComponent.topNode, config.checkable);
+    sharkComponent.component = TreeDom.initDom(sharkComponent.topNode, config);
     if (targetElement) {
         targetElement.append(sharkComponent.component);
     }
@@ -89,7 +90,7 @@ SharkUI.sharkDTree = function (options, targetElement) {
     // reRender方法
     sharkComponent.reRender = function (nodes) {
         var newTopNode = TreeData.getTopNode(nodes, config);
-        compareAndRender(sharkComponent, newTopNode, config.checkable);
+        compareAndRender(sharkComponent, newTopNode, config);
     };
     // 全选
     sharkComponent.checkAll = function () {
@@ -103,50 +104,50 @@ SharkUI.sharkDTree = function (options, targetElement) {
     sharkComponent.reverseCheck = function () {
         var newTopNode = SharkUI.extend({}, sharkComponent.topNode);
         TreeData.reverseCheck(newTopNode, newTopNode);
-        compareAndRender(sharkComponent, newTopNode, config.checkable);
+        compareAndRender(sharkComponent, newTopNode, config);
     };
     // 设置某几个节点为选中的(replace，替换掉原来的选中值)
     sharkComponent.setChecked = function (idList, replace) {
         var newTopNode = SharkUI.extend({}, sharkComponent.topNode);
         TreeData.setChecked(newTopNode, idList, true, replace, config);
-        compareAndRender(sharkComponent, newTopNode, config.checkable);
+        compareAndRender(sharkComponent, newTopNode, config);
     };
+    // 设置某几个节点为不选中的
     sharkComponent.setUnChecked = function (idList) {
         var newTopNode = SharkUI.extend({}, sharkComponent.topNode);
         TreeData.setChecked(newTopNode, idList, false, config);
-        compareAndRender(sharkComponent, newTopNode, config.checkable);
+        compareAndRender(sharkComponent, newTopNode, config);
     };
     // 设置某几个节点为选中的
     sharkComponent.setSelected = function (idList, replace) {
         var newTopNode = SharkUI.extend({}, sharkComponent.topNode);
         TreeData.setSelected(newTopNode, idList, replace, config);
-        compareAndRender(sharkComponent, newTopNode, config.checkable);
+        compareAndRender(sharkComponent, newTopNode, config);
     };
     // 展开全部
     sharkComponent.openAll = function () {
         var newTopNode = SharkUI.extend({}, sharkComponent.topNode);
         TreeData.openAll(newTopNode);
-        compareAndRender(sharkComponent, newTopNode, config.checkable);
+        compareAndRender(sharkComponent, newTopNode, config);
     };
     // 展开某几个节点(autoOpenParent:打开子节点后是否自动打开父节点)
     sharkComponent.openTo = function (idList, autoOpenParent) {
         var newTopNode = SharkUI.extend({}, sharkComponent.topNode);
         TreeData.openTo(newTopNode, idList, autoOpenParent);
-        compareAndRender(sharkComponent, newTopNode, config.checkable);
+        compareAndRender(sharkComponent, newTopNode, config);
     };
-
-    sharkComponent.setDisabled = function(idList) {
+    // 设置某几个节点为禁用的
+    sharkComponent.setDisabled = function (idList) {
         var newTopNode = SharkUI.extend({}, sharkComponent.topNode);
         TreeData.setDisabled(newTopNode, idList);
-        compareAndRender(sharkComponent, newTopNode, config.checkable);
+        compareAndRender(sharkComponent, newTopNode, config);
     };
-
-    sharkComponent.setDisabledCheckBox = function(idList) {
+    // 设置某几个checkbox为禁用的
+    sharkComponent.setDisabledCheckBox = function (idList) {
         var newTopNode = SharkUI.extend({}, sharkComponent.topNode);
         TreeData.setDisabledCheckBox(newTopNode, idList);
-        compareAndRender(sharkComponent, newTopNode, config.checkable);
+        compareAndRender(sharkComponent, newTopNode, config);
     };
-
     // 获取选中的id列表
     sharkComponent.getChecked = function () {
         if (config.checkable) {
--- src/main/webapp/scripts/src/components/d-tree/dom.js
+++ src/main/webapp/scripts/src/components/d-tree/dom.js
@@ -16,7 +16,7 @@ function toggleChildTree(treeNode, open) {
     }
 }
 
-// 修改复选框的状态
+// 切换复选框的状态
 function toggleCheckBox(checkbox, state) {
     if (checkbox) {
         checkbox.removeClass('tree-icon-check-empty tree-icon-check-minus tree-icon-check');
@@ -29,27 +29,30 @@ function toggleCheckBox(checkbox, state) {
     }
 }
 
-function toggleSelected(title, selected) {
-    title.removeClass('tree-node-selected');
-    if (selected) {
-        title.addClass('tree-node-selected');
+// 切换选中状态（节点本身）
+function toggleSelected(title, selected, config) {
+    if (config.selectable) {
+        title.removeClass('tree-node-selected');
+        if (selected) {
+            title.addClass('tree-node-selected');
+        }
     }
 }
 
 // 生成树节点的dom
-function getTreeNode(nodeData, checkable) {
+function getTreeNode(nodeData, config) {
     var treeNode = $('<li></li>');
     var title = $('<span class="tree-title tree-node-name">' + nodeData.name + '</span>');
     var children = nodeData.children;
     if (nodeData.disabled) {
         title.addClass('disabled');
     }
-    if (nodeData.selected) {
+    if (config.selectable && nodeData.selected) {
         title.addClass('tree-node-selected');
     }
     treeNode.append(title);
     treeNode.data('id', nodeData.id);
-    if (checkable) {
+    if (config.checkable) {
         var checkbox = $('<span class="tree-checkbox tree-icon"></span>');
         toggleCheckBox(checkbox, nodeData.state);
         if (nodeData.disabledCheckbox) {
@@ -58,7 +61,7 @@ function getTreeNode(nodeData, checkable) {
         treeNode.prepend(checkbox);
     }
     if (children) {
-        var childTree = getChildTree(children, nodeData.open, checkable);
+        var childTree = getChildTree(children, nodeData.open, config);
         // 存在子树则在节点前添加展开收起子树的按钮
         treeNode.prepend('<span class="tree-switcher tree-icon"></span>');
         treeNode.append(childTree);
@@ -68,25 +71,25 @@ function getTreeNode(nodeData, checkable) {
 }
 
 // 生成子树的dom
-function getChildTree(nodesData, open, checkable) {
+function getChildTree(nodesData, open, config) {
     var tree = $('<ul></ul>');
     nodesData.forEach(function (nodeData) {
-        var treeNode = getTreeNode(nodeData, checkable);
+        var treeNode = getTreeNode(nodeData, config);
         tree.append(treeNode);
     });
     return tree;
 }
 
 // 根据根数据根节点，初始化树组件的dom结构
-function initDom(topNode, checkable) {
+function initDom(topNode, config) {
     var container = $('<div class="shark-d-tree shark-tree"></div>');
-    var tree = getChildTree(topNode.children, topNode.open, checkable);
+    var tree = getChildTree(topNode.children, topNode.open, config);
     container.append(tree);
     return container;
 }
 
 // 重新排序子节点
-function reOrderChildren(node, moves, checkable) {
+function reOrderChildren(node, moves, config) {
     moves.forEach(function (move) {
         var childTree = $(node).children('ul');
         var nodeList = childTree.children('li');
@@ -96,7 +99,7 @@ function reOrderChildren(node, moves, checkable) {
             treeNode.remove();
         } else if (move.type === 1) {
             var item = move.item;
-            var treeNode = getTreeNode(item, checkable);
+            var treeNode = getTreeNode(item, config);
             if (index) {
                 nodeList.eq(index - 1).after(treeNode);
             } else {
@@ -107,48 +110,45 @@ function reOrderChildren(node, moves, checkable) {
 }
 
 // 根据得到的当前节点的差异，修改当前节点
-function applyPatches(node, currentPatches, checkable) {
+function applyPatches(node, currentPatches, config) {
+    var title = node.children('.tree-title');
+    var checkbox = node.children('.tree-checkbox');
     currentPatches.forEach(function (currentPatch) {
         switch (currentPatch.type) {
             case "REORDER":
-                reOrderChildren(node, currentPatch.moves, checkable);
+                reOrderChildren(node, currentPatch.moves, config);
                 break;
             case "NAME":
-                var title = node.children('.tree-title');
                 title.text(currentPatch.name);
                 break;
             case "STATE":
-                var checkbox = node.children('.tree-checkbox');
                 toggleCheckBox(checkbox, currentPatch.state);
                 break;
             case "SELECTED":
-                var title = node.children('.tree-title');
-                toggleSelected(title, currentPatch.selected);
+                toggleSelected(title, currentPatch.selected, config);
                 break;
             case "OPEN":
                 toggleChildTree(node, currentPatch.open);
                 break;
             case "DISABLED":
-                var title = node.children('.tree-title');
                 disabledNode(title, currentPatch.disabled);
                 break;
             case "DISABLEDCHECKBOX":
-                var checkbox = node.children('.tree-checkbox');
                 disabledCheckbox(checkbox, currentPatch.disabledCheckbox);
                 break;
         }
     });
 }
 
-
+// 将节点状态设为disabled
 function disabledNode(title, disabled) {
     title.removeClass('disabled');
     if (disabled) {
         title.addClass('disabled');
     }
-
 }
 
+// 将checkbox状态设为disabled
 function disabledCheckbox(checkbox, disabledCheckbox) {
     checkbox.removeClass('disabled');
     if (disabledCheckbox) {
@@ -157,14 +157,14 @@ function disabledCheckbox(checkbox, disabledCheckbox) {
 }
 
 // 根据得到的差异数组，修改组件
-function applyToTree(node, walker, patches, checkable) {
+function applyToTree(node, walker, patches, config) {
     var currentPatches = patches[walker.index];
     var treeNodes = node.children('ul').children('li');
     treeNodes.each(function (i, treeNode) {
         walker.index++;
-        applyToTree($(treeNode), walker, patches, checkable);
+        applyToTree($(treeNode), walker, patches, config);
     });
-    currentPatches && applyPatches(node, currentPatches, checkable);
+    currentPatches && applyPatches(node, currentPatches, config);
 }
 
 var TreeDom = {
